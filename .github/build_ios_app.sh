ab501a88d14aef0808f4d6d52558458709724776
Eugenio Amato <eugenio.amato@pinch.nl>
2021-09-12 16:55:50 +0200

    adjusted ios build

--- .github/build_ios_app.sh
+++ .github/build_ios_app.sh
@@ -2,7 +2,7 @@
 cd ios || return
 pod install
 cd ..
-flutter build ios --no-codesign --flavor=development
+flutter build ios --no-codesign --dart-define=BASE_URL=${{ secrets.DEV_BASE_URL }} --dart-define=EXEC_ENV='github_actions'
 cd build/ios/iphoneos || return
 mkdir Payload
 cd Payload || return
