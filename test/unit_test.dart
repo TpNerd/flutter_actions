5b34d88fc655920a994f2dbd9b884aef41d60390
Eugenio Amato <eugenio.amato@pinch.nl>
2021-09-12 12:35:13 +0200

    released screens on drives

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -1,21 +1,9 @@
-import 'dart:io';
-
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Hello Adapta', () {
-    test('Just print base url', () async {
-      String? platformBaseUrl = Platform.environment['BASE_URL'];
-      String? platExec = Platform.environment['EXEC_ENV'];
-
-      const baseUrl = String.fromEnvironment('BASE_URL');
-      const envExec = String.fromEnvironment('EXEC_ENV');
-
-      print('platformBaseUrl from platform: $platformBaseUrl');
-      print('platExec $platExec');
-
-      print('baseUrl from env: $baseUrl');
-      print('envExec $envExec');
+  group('flutter actions', () {
+    test('basic math test', () async {
+      expect(2 + 2, 4);
     });
   });
 }
--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
@@ -7,6 +9,18 @@ void main() {
     final buttonFinder = find.byTooltip('Increment');
 
     late FlutterDriver driver;
+
+    Future<void> takeScreenshot(String targetFile) async {
+      await driver.waitUntilNoTransientCallbacks();
+
+      // Take the screenshot and store as a list of ints
+      // Note: the image will be returned as a PNG
+      final List<int> screenshotPixels = await driver.screenshot();
+
+      // Write to a file
+      final File screenshotFile = File(targetFile);
+      await screenshotFile.writeAsBytes(screenshotPixels);
+    }
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
@@ -18,11 +32,13 @@ void main() {
 
     test('starts at 0', () async {
       expect(await driver.getText(counterTextFinder), '0');
+      await takeScreenshot('0.png');
     });
 
     test('increments the counter', () async {
       await driver.tap(buttonFinder);
       expect(await driver.getText(counterTextFinder), '1');
+      await takeScreenshot('1.png');
     });
   });
 }
