fac9abb82c50c34e992ed68d07507ba8e837578b
Eugenio Amato <eugenio.amato@pinch.nl>
2021-09-12 15:45:15 +0200

    showing base_url on page

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              const Text(String.fromEnvironment('BASE_URL')),
               Image.network(_userImage),
               const Text(
                 'You have pushed the button this many times:',
